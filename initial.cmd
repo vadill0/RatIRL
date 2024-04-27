7c08d5a7c6f2004ca1132289d3fdcccce733d0e7
vadill0 <aitorvadillo70@gmail.com>
2024-04-27 17:18:49 +0200

    create popup && cd i/o

--- initial.cmd
+++ initial.cmd
@@ -3,3 +3,14 @@
 @REM author vadill0
 
 @REM variables
+set "var=%cd%"
+
+@REM cd into directory
+cd C:/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup
+
+@REM write payloads
+(echo MsgBox "Line 1" ^& vbCrLf ^& "Line 2",262192, "Title")> popup.vbs
+
+@REM cd back to original directory
+cd "%var%"
+(echo MsgBox "Line 1" ^& vbCrLf ^& "Line 2",262192, "Title")> popup.vbs
