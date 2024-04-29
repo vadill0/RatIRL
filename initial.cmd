fbcc48e71c3c273f9246063672c27eb61cdeb176
vadill0 <aitorvadillo70@gmail.com>
2024-04-29 19:46:21 +0200

    variable name format

--- initial.cmd
+++ initial.cmd
@@ -3,14 +3,14 @@
 @REM author vadill0
 
 @REM variables
-set "var=%cd%"
-
-@REM cd into directory
-cd C:/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup
+set "VAR=%cd%"
+set "STARTUP=C:/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup"
+@REM cd STARTUP directory
+cd "%STARTUP%"
 
 @REM write payloads
 (echo MsgBox "Line 1" ^& vbCrLf ^& "Line 2",262192, "Title")> popup.vbs
 
 @REM cd back to original directory
-cd "%var%"
+cd "%VAR%"
 del initial.cmd
