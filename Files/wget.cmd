f878845e010cc4448af0e157afd3c3ac761064e9
vadill0 <aitorvadillo70@gmail.com>
2024-05-29 18:54:47 +0200

    admin scrpit fixed

--- Files/wget.cmd
+++ Files/wget.cmd
@@ -3,29 +3,29 @@ REM get admin permissions for script
 :: BatchGotAdmin
 
 REM -- check for permissions
-IF "PROCESSOR ARCHITECTURE%" EQU "amd64" (
->nul 2>&1 "SYSTEMROOT\SysWOW64\cacls.exe" "%SYSTEMROOT\SysWOW64\config\system"
+IF "%PROCESSOR ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
 ) ELSE (
->nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "SYSTEMROOT\system32\config\system"
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 )
 
 REM --  if error flag set, we do not have admin.
-if 'errorlevel%' NEQ '0' (
+if '%errorlevel%' NEQ '0' (
 echo Requesting administrative privileges ...
 goto UACPrompt
 ) else ( goto gotAdmin )
 
 :UACPrompt
-echo Set UAC = CreateObject^("Shell.Application"^) > "temp\getadmin.vbs"
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
 set params= %*
-echo UAC. ShellExecute "cmd. exe", "/c ""~s0"" $params: "=""", "", "runas", 1 >> "temp\getadmin.vbs
+echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
 
 "%temp%\getadmin.vbs"
-del "temp\getadmin.vbs"
+del "%temp%\getadmin.vbs"
 exit /B
 
 :gotAdmin
-pushd "%CD"
+pushd "%CD%"
 CD /D "%~dp0"
 
 REM you can remove the 'powershell' to get an admin CMD
